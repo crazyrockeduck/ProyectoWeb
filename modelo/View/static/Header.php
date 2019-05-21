c4cfd7aa3212710bb9edef349ebfa7bcaf4919bd
crazyrockeduck <50890602+crazyrockeduck@users.noreply.github.com>
2019-05-21 17:55:57 -0600

    Update Header.php

--- modelo/View/static/Header.php
+++ modelo/View/static/Header.php
@@ -45,10 +45,10 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="<?=$GLOBALS['COD']->dir?>/View/home">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="<?=$GLOBALS['COD']->dir?>/home">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="<?=$GLOBALS['COD']->dir?>/View/contacto">Contacto <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="<?=$GLOBALS['COD']->dir?>/contacto">Contacto <span class="sr-only">(current)</span></a>
       </li>
     </ul>
   </div>
